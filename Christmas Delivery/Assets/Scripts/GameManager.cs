92aeaa8b962dcee7c72d39bf171734f50085dfd5
RoyalMelonStudios <royalmelonstudios@gmail.com>
2020-10-22 18:06:04 -0500

    Fixed Basic Touch Controls and set to 60 FPS

--- Christmas Delivery/Assets/Scripts/GameManager.cs
+++ Christmas Delivery/Assets/Scripts/GameManager.cs
@@ -32,6 +32,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+        Application.targetFrameRate = 60;
         StartCoroutine(SpawnToys());
     }
  
